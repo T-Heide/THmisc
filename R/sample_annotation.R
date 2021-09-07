c9456af6aa69c16eb8dd867df5a5db57c01ebcf2
Timon Heide <timon.heide@icr.ac.uk>
2021-09-08 01:11:11 +0200

    Allow fast annot. of multiple duplicated barcodes.

--- R/sample_annotation.R
+++ R/sample_annotation.R
@@ -1,6 +1,6 @@
 #' Get sample annotations from sample barcodes.
 #'
-#' @param barcodes A character vector of sample barcodes.
+#' @param barcodes A character (or factor) vector of sample barcodes.
 #' @param extract Logical flag if barcodes should be extracted from 'barcodes' (Default: False).
 #'
 #' @return A data frame with sample annotations, rownames are set if there are no duplicated barcodes. 
@@ -10,12 +10,21 @@
 annotation_from_barcode = function(barcodes, extract=FALSE) {
   
   # quick check of input
+  if (is.factor(barcodes)) barcodes = as.character(barcodes)
   checkmate::assertCharacter(barcodes, null.ok = FALSE)
   checkmate::assertFlag(extract)
+  
+  # if any barcodes are duplicated only annotate unique barcodes
+  if (any(duplicated(barcodes))) {
+    annot = annotation_from_barcode_epicc(unique(barcodes))[barcodes,]
+    rownames(annot) = NULL
+    return(annot)
+  }
   
   # try annotation with EPICC project barcodes
   try({
     annot = annotation_from_barcode_epicc(barcodes, extract)
+    rownames(annot) = barcodes
     return(annot)
   }, silent = TRUE)
   
