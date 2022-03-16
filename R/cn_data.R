696d7921f41f190e4c0636b16c7af64ce1cac52e
Timon Heide <timon.heide@icr.ac.uk>
2022-03-16 16:50:04 +0100

    Adds function to retrieve A&B number from CNA annotations.

--- R/cn_data.R
+++ R/cn_data.R
@@ -75,3 +75,72 @@ get_cnas = function(mutation_data, cna_segments, sample_ids=NULL) {
   return(cna_status)
 }
 
+#' Get A&B allele numbers from a list of CN data.
+#'
+#' @param mutation_data A CollapsedVCF object, GRanges object or a mutation id.
+#' @param cna_segments A (named) list of CN segment data that contain A and B allele data.
+#' @param sample_ids Indices of samples from 'cna_segments' to include in the output.
+#'
+#' @return A matrix containing annotations for each mutation object.
+#' @export
+#'
+get_ab_alleles = function(mutation_data, cna_segments, sample_ids=NULL) {
+
+  if ("CollapsedVCF" %in% class(mutation_data)) {
+    sample_ids = colnames(mutation_data)
+    chrs = as.character(GenomicRanges::seqnames(mutation_data))
+    pos = GenomicRanges::start(mutation_data)
+  }
+
+  if (!is.null(sample_ids)) {
+    chrs = as.character(GenomicRanges::seqnames(mutation_data))
+    pos = GenomicRanges::start(mutation_data)
+  }
+
+  #
+  #
+  # else if ("data.frame" %in% class(mutation_data)) {
+  #   stop("Invalid input.\n")
+  #
+  #   if (!all(c("chr","pos") %in% colnames(mutation_data))) {
+  #     stop("Invalid input.\n")
+  #   }
+  #   sample_ids = "AB"
+  #   chrs = mutation_data
+  #   pos = ""
+  # } else {
+  #   stop("Invalid input.\n")
+  # }
+
+
+  # check for missing segment data:
+  sample_with_cnas = sample_ids %in% names(cna_segments)
+  if (any(!sample_with_cnas)) {
+    missing = paste0(sample_ids[!sample_with_cnas], collapse=", ")
+    warning("Missing segment data: ", missing, ".\n", sep="")
+  }
+
+  # fill cna matrix:
+  cna_status = matrix(NA, NROW(mutation_data), length(sample_ids))
+  dimnames(cna_status) = list(rownames(mutation_data), sample_ids)
+
+  for (i in which(sample_with_cnas)) { # each sample
+    sample_cna = cna_segments[[sample_ids[i]]] # samples segment data
+
+    for (j in seq_len(NROW(sample_cna))) { # each cna segment
+      wh = chrs == sample_cna$chromosome[j] &
+        between(pos, sample_cna$start.pos[j], sample_cna$end.pos[j])
+
+      if (is.na(sample_cna$A[j]) | is.na(sample_cna$B[j])) {
+        ab_status = NA
+      } else {
+        a_count = rep("A", sample_cna$A[j])
+        b_count = rep("B", sample_cna$B[j])
+        ab_status = paste0(c(a_count, b_count), collapse="")
+      }
+      cna_status[wh,i] = ab_status
+    }
+  }
+
+  return(cna_status)
+}
